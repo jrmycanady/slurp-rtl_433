9359f6c07801688e4b550e749ebfe7a32e42dbae
Jeremy Canady <jcanady@gmail.com>
2018-07-17 07:53:29 -0500

    d

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./slurp-rtl_433 --data-location=./test/logs/rtl_433_short.log --meta-data-location=./test/meta -D -v
+./slurp-rtl_433 --data-location=./test/logs/rtl_433_short.log --meta-data-location=./test/meta -D -v -f 192.162.20.252 -u data_dump_influx_user -p '#kndnfe$h2243'
